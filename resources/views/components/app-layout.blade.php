d93b617ee5bd286b0705821f5532f7383012041d
Akshay Kumar <akshay.kumar@cloudanalogy.com>
2025-01-09 19:44:37 +0530

    Setup the Home page and layouts components

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,6 @@
+@props(["title"])
+<x-base-layout :$title>
+    <x-header />
+    {{ $slot }}
+    <x-footer></x-footer>
+</x-base-layout>
---
+++ resources/views/components/base-layout.blade.php
@@ -0,0 +1,33 @@
+@props(["bodyClass" => "", "title"])
+<!DOCTYPE html>
+<html lang="{{ config("app.locale") }}">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ $title }}| {{ config("app.name") }}</title>
+
+    <link href="https://fonts.googleapis.com" rel="preconnect" />
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"
+        integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link
+href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet"/> -->
+
+    <link href="css/app.css" rel="stylesheet" />
+    <!-- <link href="css/output.css" rel="stylesheet"/> -->
+</head>
+
+<body>
+    {{ $slot }}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"
+        integrity="sha512-XJgPMFq31Ren4pKVQgeD+0JTDzn0IwS1802sc+QTZckE6rny7AN2HLReq6Yamwpd2hFe5nJJGZLvPStWFv5Kww=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
